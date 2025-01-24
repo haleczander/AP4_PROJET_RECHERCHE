3d3567a52b3cf39df6af057f64a09edfbc61afc1
Alexandre HERSSENS <alexande.herssens@student.junia.com>
2025-01-24 10:55:07 +0100

    DEV#79 - Activation utils : dev + base des tests

---
+++ src/src/utils/activations.utils.js
@@ -0,0 +1,24 @@
+import { ActivationReaction } from "../models/activation.reaction.model";
+
+export function getEnergieKWh( activation ) {
+    const dureeM = activation.dureeM;
+    const puissanceW = activation.puissance;
+    const puissanceKw = puissanceW / 1000; 
+
+    return ( dureeM * puissanceKw ) / 60;
+}
+
+export function getPrixEnergie( activation, prixKWh ) {
+    const energieKWh = getEnergieKWh( activation );
+
+    return energieKWh * prixKWh;
+}
+
+export function createActivationReaction( activation  ) {
+    const activationReaction = new ActivationReaction( 
+        null, 
+        activation.nom 
+    );
+
+    return activation;
+}
