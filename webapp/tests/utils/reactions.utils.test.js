3ef135e4d22548ea938cdfa36d38f738ad7a3b3a
Alexandre Herssens <alex.herssens@gmail.com>
2025-02-04 12:11:05 +0100

    TEST#92 - Test initial de la fonction utilisée globalement

---
+++ webapp/tests/utils/reactions.utils.test.js
@@ -0,0 +1,13 @@
+import { getSum } from "../../src/utils/reactions.utils";
+
+test(
+    "test fonction getSum( [], fn )",
+    () => {
+        const elements = [ 1, 3, 5, 7, 10 ];
+        expect( 
+            getSum( elements, e => e )
+        ).toBe(
+            1+3+5+7+10
+        );
+    }
+)
