f72f36778fee479a9d944ba440bbc7fac79c2abc
Clément GZ <clementgrez@gmail.com>
2025-02-18 14:58:34 +0100

    Fix divers

--- webapp/src/indicateurs/impl/masseDechets.indicateur.js
+++ webapp/src/indicateurs/impl/masseDechets.indicateur.js
@@ -10,12 +10,23 @@ export class MasseDechetsIndicateur extends Indicateur {
     }
 
     reactionPrincipale( reaction ){
+        /*
+        J10-F59
+        J10 = Somme masse reactifs
+        F59 = masse produit
+        */
         const masseReactifs = this.reactionService.masseReactifs( reaction );
         const masseProduit = reaction.produit.masseG;
         return masseReactifs - masseProduit;
     };
 
     reactionComplete( reaction ){
+        /*
+        D55-F59-F55
+        D55 = masse totale de reaction
+        F59 = masse produit
+        F55 = masse recyclable
+        */
         const masseReactionComplete = this.reactionService.masseReactionComplete( reaction );
         const masseRecyclable = this.reactionService.masseRecyclable( reaction );
         
