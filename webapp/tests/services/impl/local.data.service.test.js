e16796c26da0141a696e04a1b49df4e0b5f5bd28
Alexandre HERSSENS <alexande.herssens@student.junia.com>
2025-02-20 13:19:57 +0100

    fix chemins tests localdataserv

--- webapp/tests/services/impl/local.data.service.test.js
+++ webapp/tests/services/impl/local.data.service.test.js
@@ -1,5 +1,5 @@
 import { LocalDataService } from "../../../src/services/impl/local.data.service";
-import { BENZALDEHYDE, UREE, ACETOACETATE_ETHYLE, ACIDE_CHLORHYDRIQUE, ETHANOL, EAU } from "../../molecules.data";
+import { BENZALDEHYDE, UREE, ACETOACETATE_ETHYLE, ACIDE_CHLORHYDRIQUE, ETHANOL, EAU } from "../../data/molecules.data";
 
 describe("LocalDataService", () => {
     let service;
