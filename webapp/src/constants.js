2b7e6cacb98dbdb943cea51baea3424d7edf9606
Alexandre Herssens <alex.herssens@gmail.com>
2025-05-15 05:27:57 +0200

    FIX: DEV - Ajout des dangers H au formulaire d'ajout des molécules

--- webapp/src/constants.js
+++ webapp/src/constants.js
@@ -3,4 +3,6 @@ export const REGEX_FORMULA = /^([A-Z][a-z]*\d*)+$/;
 export const REGEX_CAS = /^\d{2,7}-\d{2}-\d{1}$/;
 export const REGEX_INTEGER = /^-?\d+$/;
 export const REGEX_FLOAT = /^-?\d+(\.\d+)?$/;
+export const REGEX_DANGER_H = /\bH\d{3}\b/;
+export const REGEX_DANGER_H_LIST = /^(\s*\bH\d{3})*\b\s*$/;
 export const REGEX_FLOAT_nF = (n) => new RegExp(`^-?\\d+(?:\\.\\d{1,${n}})$`);
